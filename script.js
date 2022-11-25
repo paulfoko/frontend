4908be46eee9f3d4897ada81bf88ba0161f3fea6
paulfoko <juniorfoko31@gmail.com>
2022-11-25 03:36:59 +0100

    test

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
 let img_slider = document.getElementsByClassName('container_img');
 let text_slider = document.getElementsByClassName('text');
 let etape = 0;
